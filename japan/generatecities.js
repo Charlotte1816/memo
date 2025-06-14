358b89aa24cc5e30389b761f50f7bd19046898b7
Charlotte1816 <integ8983dx@outlook.jp>
2025-06-14 22:48:05 +0900

    1

--- japan/generatecities.js
+++ japan/generatecities.js
@@ -6,167 +6,208 @@ const path = require('path');
 // 各都道府県の名称（表示名）とその市の一覧を定義
 const data = {
     "iwate": {
-        prefectureName: "岩手県",
+        prefectureName: "iwate",
+        prefectureNameEn: "iwate",
         cities: ["morioka", "miyako", "ofunato", "kuji", "rikuzentakata", "kamaishi", "hanamaki", "kitakami", "ninohe", "hachimantai", "tono", "ichinoseki", "oshu", "takizawa"]
     },
     "miyagi": {
-        prefectureName: "宮城県",
+        prefectureName: "miyagi",
+        prefectureNameEn: "miyagi",
         cities: ["sendai", "ishinomaki", "shiogama", "kesennuma", "shiroishi", "natori", "kakuda", "tagajo", "iwanuma", "tome", "kurihara", "higashimatsushima", "osaki", "tomiya"]
     },
     "yamagata": {
-        prefectureName: "山形県",
+        prefectureName: "yamagata",
+        prefectureNameEn: "yamagata",
         cities: ["yamagatashi", "tsuruoka", "sakata", "shinjo", "yonezawa", "tendo", "murayama", "nagai", "obanazawa", "sagae", "nanyo", "higashine", "kaminoyama"]
     },
     "fukushima": {
-        prefectureName: "福島県",
+        prefectureName: "fukushima",
+        prefectureNameEn: "fukushima",
         cities: ["fukushimashi", "koriyama", "iwaki", "aizuwakamatsu", "shirakawa", "sukagawa", "kitakata", "nihonmatsu", "tamura", "minamisoma", "date", "soma", "motomiya"]
     },
     "ibaraki": {
-        prefectureName: "茨城県",
+        prefectureName: "ibaraki",
+        prefectureNameEn: "ibaraki",
         cities: ["mito", "hitachi", "tsuchiura", "tsukuba", "hitachinaka", "kashima", "ryugasaki", "toride", "itako", "yuki", "koga", "bando", "hokota", "shimotsuma", "kasama", "hitachiota", "joso", "kamisu", "namegata", "chikusei", "sakuragawa", "omitama", "takahagi", "kitaibaraki", "inan", "nasu", "moriyama", "ogawa", "chiyoda", "mihara", "kawakami", "yamato"]
     },
     "tochigi": {
-        prefectureName: "栃木県",
-        cities: ["utsunomiya", "ashikaga", "tochigi", "sano", "kanuma", "nikko", "oyama", "mooka", "otawara", "yaita", "nasushiobara", "sakura", "nasukarasuyama", "shimotsuke"]
+        prefectureName: "tochigi",
+        prefectureNameEn: "tochigi",
+        cities: ["utsunomiya", "ashikaga", "tochigishi", "sano", "kanuma", "nikko", "oyama", "mooka", "otawara", "yaita", "nasushiobara", "sakura", "nasukarasuyama", "shimotsuke"]
     },
     "gunma": {
-        prefectureName: "群馬県",
+        prefectureName: "gunma",
+        prefectureNameEn: "gunma",
         cities: ["maebashi", "takasaki", "ota", "isessaki", "kiryu", "shibukawa", "fujioka", "tomigusuku", "numata", "annaka", "tomioka", "midori"]
     },
     "tokyo": {
-        prefectureName: "東京都",
+        prefectureName: "tokyo",
+        prefectureNameEn: "tokyo",
         cities: ["hachioji", "tachikawa", "musashino", "mitaka", "ome", "fuchu", "akishima", "chofu", "machida", "koganei", "kodaira", "hino", "higashimurayama", "kokubunji", "kunitachi", "fussa", "komae", "higashiyamato", "kiyose", "higashikurume", "musashimurayama", "tama", "inagi", "hamura", "akiruno", "nishitokyo"]
     },
     "kanagawa": {
-        prefectureName: "神奈川県",
+        prefectureName: "kanagawa",
+        prefectureNameEn: "kanagawa",
         cities: ["yokohama", "kawasaki", "sagamihara", "yokosuka", "hiratsuka", "kamakura", "fujisawa", "odawara", "chigasaki", "zushi", "miura", "hadano", "atsugi", "yamato", "isehara", "ebina", "zama", "ayase", "minamiashigara"]
     },
     "niigata": {
-        prefectureName: "新潟県",
+        prefectureName: "niigata",
+        prefectureNameEn: "niigata",
         cities: ["niigatashi", "nagaoka", "joetsu", "sanjo", "kashiwazaki", "shibata", "tokamachi", "mitsuke", "murakami", "tsubame", "gosen", "oajiya", "agano", "uonuma", "minamiuonuma", "sado", "kamo", "myoko", "itoigawa", "seiro"]
     },
     "toyama": {
-        prefectureName: "富山県",
+        prefectureName: "toyama",
+        prefectureNameEn: "toyama",
         cities: ["toyamashi", "takaoka", "uotoyama", "imizu", "nanto", "tonami", "oyabe", "himi", "kurobe", "namerikawa"]
     },
     "ishikawa": {
-        prefectureName: "石川県",
+        prefectureName: "ishikawa",
+        prefectureNameEn: "ishikawa",
         cities: ["kanazawa", "nanao", "komatsu", "wajima", "suzu", "kaga", "hakui", "kahoku", "hakusan", "nonoichi", "nonoichi"]
     },
     "fukui": {
-        prefectureName: "福井県",
+        prefectureName: "fukui",
+        prefectureNameEn: "fukui",
         cities: ["fukuishi", "tsuruga", "sabae", "obama", "ono", "katsuyama", "awara", "echizen", "minamiechizen"]
     },
     "yamanashi": {
-        prefectureName: "山梨県",
+        prefectureName: "yamanashi",
+        prefectureNameEn: "yamanashi",
         cities: ["kofu", "fujiyoshida", "tsuru", "uenohara", "nirasaki", "minamiarupusu", "hokuto", "koshu", "chuo", "fuefuki", "yamanashishi", "kai"]
     },
     "nagano": {
-        prefectureName: "長野県",
+        prefectureName: "nagano",
+        prefectureNameEn: "nagano",
         cities: ["naganoshi", "matsumoto", "ueda", "okaya", "suwa", "chikuma", "komoro", "saku", "shiojiri", "iida", "azumino", "omachi", "ina", "komagane", "iiyama", "chino", "tomi", "nagano", "nagano"]
     },
     "gifu": {
-        prefectureName: "岐阜県",
+        prefectureName: "gifu",
+        prefectureNameEn: "gifu",
         cities: ["gifushi", "ogaki", "takayama", "kakamigahara", "minokamo", "kani", "mizunami", "ena", "soki", "tajimi", "gujo", "hida", "mizuho", "hashima", "motosu", "nakatsugawa", "seki", "yamagata", "kaizu", "godo", "yoro"]
     },
     "shizuoka": {
-        prefectureName: "静岡県",
+        prefectureName: "shizuoka",
+        prefectureNameEn: "shizuoka",
         cities: ["shizuokashi", "hamamatsu", "numazu", "fuji", "fujinomiya", "mishima", "ito", "atami", "gotemba", "fukuroi", "iwata", "kikugawa", "kosai", "makinohara", "shimoda", "susono", "yaizu", "kakegawa", "omaezaki", "iwata", "shimada", "yaizu", "shimoda"]
     },
     "mie": {
-        prefectureName: "三重県",
+        prefectureName: "mie",
+        prefectureNameEn: "mie",
         cities: ["tsu", "yokkaichi", "iseshi", "matsusaka", "kameyama", "kuwana", "iga", "inabe", "owase", "kumano", "toba", "shiroko", "suzuka", "shingu"]
     },
     "shiga": {
-        prefectureName: "滋賀県",
+        prefectureName: "shiga",
+        prefectureNameEn: "shiga",
         cities: ["otsu", "hikone", "nagahama", "omihachiman", "kusatsu", "moriyama", "ritto", "yasu", "konan", "koka", "higashiomi", "takashima", "maibara"]
     },
     "kyoto": {
-        prefectureName: "京都府",
+        prefectureName: "kyoto",
+        prefectureNameEn: "kyoto",
         cities: ["kyotoshi", "fukuchiyama", "maizuru", "ayabe", "kameoka", "miyazu", "muko", "nagaokakyo", "yawata", "uji", "joetsu", "kyotango", "nantei", "ide", "seika"]
     },
     "osaka": {
-        prefectureName: "大阪府",
+        prefectureName: "osaka",
+        prefectureNameEn: "osaka",
         cities: ["osakashi", "sakai", "ibaraki", "takatsuki", "moriguchi", "hirakata", "neyagawa", "katano", "settsu", "suita", "toyonaka", "ikeda", "matsubara", "daito", "kashiwara", "kadoma", "fujiidera", "habikino", "higashiosaka", "kishiwada", "kaizuka", "izumisano", "izumi", "takaishi", "sennan", "osakasayama", "tondabayashi", "kawachinagano", "minoh", "shijonawate", "taishi", "yoshinogawa", "yamatotakada"]
     },
     "hyogo": {
-        prefectureName: "兵庫県",
+        prefectureName: "hyogo",
+        prefectureNameEn: "hyogo",
         cities: ["kobe", "himeji", "akash", "amagasaki", "nishinomiya", "ashiya", "akashi", "sumoto", "kakogawa", "takarazuka", "miki", "ono", "nishiwaki", "tanba", "kasai", "sasayama", "fukusaki", "aioi", "sanda", "tatsuno", "ako", "toyooka", "asago", "yabu", "shiso", "minamiawaji", "awaji", "kami", "shinonsen"]
     },
     "nara": {
-        prefectureName: "奈良県",
+        prefectureName: "nara",
+        prefectureNameEn: "nara",
         cities: ["narashi", "ikoma", "tenri", "kashihara", "yamatokoriyama", "yamatotakada", "sakurai", "gojo", "gose", "kashiba", "ikoma", "uda"]
     },
     "wakayama": {
-        prefectureName: "和歌山県",
+        prefectureName: "wakayama",
+        prefectureNameEn: "wakayama",
         cities: ["wakayamashi", "shingu", "tanabe", "gobo", "arida", "iwade", "hashimoto", "kinokawa", "kainan"]
     },
     "tottori": {
-        prefectureName: "鳥取県",
+        prefectureName: "tottori",
+        prefectureNameEn: "tottori",
         cities: ["tottorishi", "yonago", "kurayoshi", "sakaiminato"]
     },
     "shimane": {
-        prefectureName: "島根県",
+        prefectureName: "shimane",
+        prefectureNameEn: "shimane",
         cities: ["matsue", "izumo", "hamada", "masuda", "gotsu", "unnan", "oda"]
     },
     "okayama": {
-        prefectureName: "岡山県",
+        prefectureName: "okayama",
+        prefectureNameEn: "okayama",
         cities: ["okayamashi", "kurashiki", "tsuyama", "tamano", "soja", "setouchi", "bizen", "niimi", "ibara", "asakuchi", "takahashi", "maniwa", "kasaoka", "akasaka", "mimasaka"]
     },
     "hiroshima": {
-        prefectureName: "広島県",
+        prefectureName: "hiroshima",
+        prefectureNameEn: "hiroshima",
         cities: ["hiroshimashi", "kure", "fukuyama", "onomichi", "mihara", "higashihiroshima", "shobara", "otake", "takehara", "fuchu", "miyoshi", "etajima", "akita", "kitahiroshima"]
     },
     "yamaguchi": {
-        prefectureName: "山口県",
+        prefectureName: "yamaguchi",
+        prefectureNameEn: "yamaguchi",
         cities: ["yamaguchishi", "shimonoseki", "ube", "hagi", "iwakuni", "hikari", "nagato", "yanai", "mine", "shunan", "hofu", "sanyo-onoda", "kudamatsu"]
     },
     "tokushima": {
-        prefectureName: "徳島県",
+        prefectureName: "tokushima",
+        prefectureNameEn: "tokushima",
         cities: ["tokushimashi", "komatsushima", "an'an", "yoshinogawa", "awa", "mima", "miyoshi", "naruto"]
     },
     "kagawa": {
-        prefectureName: "香川県",
+        prefectureName: "kagawa",
+        prefectureNameEn: "kagawa",
         cities: ["takamatsu", "marugame", "sakaide", "kanonji", "zentsuji", "sanuki", "mitoyo", "higashikagawa"]
     },
     "ehime": {
-        prefectureName: "愛媛県",
+        prefectureName: "ehime",
+        prefectureNameEn: "ehime",
         cities: ["matsuyama", "imabari", "uwajima", "yawaragi", "niihama", "saijo", "ozu", "seiyo", "toon", "kumakogen", "shikokuchuo"]
     },
     "kochi": {
-        prefectureName: "高知県",
+        prefectureName: "kochi",
+        prefectureNameEn: "kochi",
         cities: ["kochishi", "nankoku", "konan", "shimanto", "tosashimizu", "sukumo", "muroto", "susaki"]
     },
     "fukuoka": {
-        prefectureName: "福岡県",
+        prefectureName: "fukuoka",
+        prefectureNameEn: "fukuoka",
         cities: ["fukuokashi", "kitakyushu", "kurume", "omuta", "itagawa", "iizuka", "kasuga", "kurate", "asakura", "yame", "ukiha", "munakata"]
     },
     "saga": {
-        prefectureName: "佐賀県",
+        prefectureName: "saga",
+        prefectureNameEn: "saga",
         cities: ["sagashi", "karatsu", "taku", "kanzaki", "tosu", "ogi"]
     },
     "nagasaki": {
-        prefectureName: "長崎県",
+        prefectureName: "nagasaki",
+        prefectureNameEn: "nagasaki",
         cities: ["nagasakishi", "sasebo", "shimabara", "isahaya", "omura", "hirado", "saikai", "goto", "unzen", "tsushima", "minamishimabara", "nishisonogi", "matsuura"]
     },
     "kumamoto": {
-        prefectureName: "熊本県",
+        prefectureName: "kumamoto",
+        prefectureNameEn: "kumamoto",
         cities: ["kumamotoshi", "arao", "tamana", "gyokuto", "koshi", "minamata", "amaki", "yamaga", "kikuchi", "kosa", "mashiki", "higo", "uki", "yamaga"]
     },
     "oita": {
-        prefectureName: "大分県",
+        prefectureName: "oita",
+        prefectureNameEn: "oita",
         cities: ["oitashishi", "beppu", "usuki", "nakatsu", "hita", "kitsuki", "saiki", "bungotakada", "yufu"]
     },
     "miyazaki": {
-        prefectureName: "宮崎県",
+        prefectureName: "miyazaki",
+        prefectureNameEn: "miyazaki",
         cities: ["miyazakishi", "nobeoka", "miyakonojo", "kobayashi", "takanabe", "nichinan", "hyuga"]
     },
     "kagoshima": {
-        prefectureName: "鹿児島県",
+        prefectureName: "kagoshima",
+        prefectureNameEn: "kagoshima",
         cities: ["kagoshimashi", "kanoya", "ichikikushikino", "satsumasendai", "tarumizu", "makurazaki", "izumi", "isa", "hioki", "minamisatsuma", "kawanabe", "kouyama", "naze", "ama"]
     },
     "okinawa": {
-        prefectureName: "沖縄県",
+        prefectureName: "okinawa",
+        prefectureNameEn: "okinawa",
         cities: ["naha", "okinawashi", "uruma", "gushikawa", "ishigaki", "nago", "ginowan", "tomigusuku", "miyakojima", "yaese", "itoman"]
     }
 };
@@ -229,7 +270,7 @@ const cityTemplate = (prefName, city) => `<!DOCTYPE html>
     <meta name="author" content="Charlotte">
     <meta name="robots" content="index, follow">
     <meta name="googlebot" content="index, follow">
-    <title>${city}市 - ${prefName}</title>
+    <title>${city}市</title>
     <link rel="stylesheet" href="/style.css">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon" href="/stained_glass.png">
@@ -241,7 +282,11 @@ const cityTemplate = (prefName, city) => `<!DOCTYPE html>
       <nav>
          <ul>
            <li><a href="/index.html">HOME</a></li>
-           <li><a href="${prefName.toLowerCase()}.html">${prefName}</a></li>
+         </ul>
+         <ul>
+           <li><a href="${prefName}.html">${prefName}</a></li>
+         </ul>
+         <ul>
            <li><a href="#top">TOP</a></li>
          </ul>
       </nav>
